7f4038ad3334caa91f0eb93e071f31f3c0b81878
someday <somedaymail@163.com>
2020-06-01 14:30:51 +0800

    优化代码

--- inner-intergration/uaa-client-spring-boot-starter/src/main/java/com/open/capacity/uaa/client/constant/AuthServiceConstant.java
+++ inner-intergration/uaa-client-spring-boot-starter/src/main/java/com/open/capacity/uaa/client/constant/AuthServiceConstant.java
@@ -11,6 +11,13 @@ package com.open.capacity.uaa.client.constant;
  */
 
 public class AuthServiceConstant {
+	
+	 /**
+     * 网关服务的serviceId
+     */
 	public static final String GATEWAY_SERVICE = "api-gateway";
+	/**
+     * 认证服务的serviceId
+     */
 	public static final String AUTH_SERVICE = "auth-server";
 }
