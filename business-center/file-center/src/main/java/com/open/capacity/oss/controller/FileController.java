f0e51062a381ec1b49ddb9e10831a044d1947d92
gitgeek1 <gitgeek@163.com>
2020-06-14 18:40:59 +0800

    大文件上传

--- business-center/file-center/src/main/java/com/open/capacity/oss/controller/FileController.java
+++ business-center/file-center/src/main/java/com/open/capacity/oss/controller/FileController.java
@@ -1,16 +1,21 @@
 package com.open.capacity.oss.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
+import com.open.capacity.oss.config.WebResourceConfig;
+import com.open.capacity.oss.model.MergeFileDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -25,6 +30,9 @@ import com.open.capacity.oss.service.FileService;
 
 import io.swagger.annotations.Api;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author 作者 owen 
  * @version 创建时间：2017年11月12日 上午22:57:51
@@ -33,11 +41,16 @@ import io.swagger.annotations.Api;
 */
 @RestController
 @Api(tags = "FILE API")
+@Slf4j
 public class FileController {
 
 	@Autowired
 	private OssServiceFactory fileServiceFactory;
 	private ObjectMapper objectMapper = new ObjectMapper();
+	@Value("${file.oss.path}")
+	private String localFilePath;
+
+
 	/**
 	 * 文件上传
 	 * 根据fileType选择上传方式
@@ -111,4 +124,91 @@ public class FileController {
 		return  fileServiceFactory.getFileService(FileType.QINIU.toString()).findList(params);
 
 	}
+
+
+
+	/**
+	 * 上传大文件
+	 * @param request
+	 * @param response
+	 * @param file
+	 * @param chunks
+	 */
+	@PostMapping(value = "/files-anon/bigFile")
+	public void bigFile(HttpServletRequest request, HttpServletResponse response, String guid, Integer chunk, MultipartFile file, Integer chunks){
+		try {
+			// 获取文件需要上传到的路径
+			String filePath = localFilePath;
+
+			log.info("guid:{},chunkNumber:{}",guid,chunk);
+
+			if(Objects.isNull(chunk)){
+				chunk = 0;
+			}
+			boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+			if (isMultipart) {
+				// 临时目录用来存放所有分片文件
+				String tempFileDir = filePath + File.separator + guid;
+				File parentFileDir = new File(tempFileDir);
+				if (!parentFileDir.exists()) {
+					parentFileDir.mkdirs();
+				}
+				// 分片处理时，前台会多次调用上传接口，每次都会上传文件的一部分到后台
+				File tempPartFile = new File(parentFileDir, guid + "_" + chunk + ".part");
+				FileUtils.copyInputStreamToFile(file.getInputStream(), tempPartFile);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@RequestMapping(value = "/files-anon/merge",method =RequestMethod.POST )
+	public Result mergeFile(@RequestBody MergeFileDTO mergeFileDTO){
+
+		String guid = mergeFileDTO.getGuid();
+		String fileName = mergeFileDTO.getFileName();
+		// 获取文件需要上传到的路径
+		String filePath = localFilePath;
+		String filePathName = filePath + File.separator + org.apache.commons.lang3.StringUtils.substringBeforeLast(fileName, ".")+ File.separator;
+		log.info("filePathName:{},fileName:{}",filePathName,fileName);
+
+
+		// 得到 destTempFile 就是最终的文件
+		log.info("guid:{},fileName:{}",guid,fileName);
+		try {
+			File parentFileDir = new File(filePath + File.separator + guid);
+			if(parentFileDir.isDirectory()){
+				File destTempFile = new File(filePath , fileName);
+				if(!destTempFile.exists()){
+					//先得到文件的上级目录，并创建上级目录，在创建文件,
+					destTempFile.getParentFile().mkdir();
+					try {
+						//创建文件
+						destTempFile.createNewFile(); //上级目录没有创建，这里会报错
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				log.info("length:{} ",parentFileDir.listFiles().length);
+
+				for (int i = 0; i < parentFileDir.listFiles().length; i++) {
+					File partFile = new File(parentFileDir, guid + "_" + i + ".part");
+					FileOutputStream destTempfos = new FileOutputStream(destTempFile, true);
+					//遍历"所有分片文件"到"最终文件"中
+					FileUtils.copyFile(partFile, destTempfos);
+					destTempfos.close();
+				}
+				// 删除临时目录中的分片文件
+				FileUtils.deleteDirectory(parentFileDir);
+
+				return Result.succeed("");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Result.failed("");
+		}
+		return null;
+	}
+
+
 }
