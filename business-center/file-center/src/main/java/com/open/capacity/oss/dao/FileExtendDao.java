6dde20b7fa911502f5dc475ebb32dfc5a8fe1794
Administrator <zhangzhiguang>
2020-06-28 18:03:56 +0800

    FASTDFS大文件上传

---
+++ business-center/file-center/src/main/java/com/open/capacity/oss/dao/FileExtendDao.java
@@ -0,0 +1,7 @@
+package com.open.capacity.oss.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FileExtendDao {
+}
---
+++ business-center/file-center/src/main/java/com/open/capacity/oss/model/FileExtend.java
@@ -0,0 +1,24 @@
+package com.open.capacity.oss.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class FileExtend implements  Serializable{
+    private static final long serialVersionUID = -3542330889450919312L;
+    //  md5字段
+    private String id;
+    //  原始文件名
+    private String name;
+    //	文件大小
+    private long size;
+    //  冗余字段
+    private String path;
+    //	oss访问路径 oss需要设置公共读
+    private String url;
+    //	FileType字段
+    private String source;
+    //	主文件id
+    private String fileId;
+}
