02894334d5911c4b9873c69eb394c493215b985e
someday <somedaymail@163.com>
2020-07-29 19:31:14 +0800

    单元测试用例

--- new-api-gateway/src/main/java/com/open/capacity/client/filter/RateLimitFilter.java
+++ new-api-gateway/src/main/java/com/open/capacity/client/filter/RateLimitFilter.java
@@ -64,7 +64,7 @@ public class RateLimitFilter implements GlobalFilter, Ordered {
             exchange.getResponse().setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
             ServerHttpResponse response = exchange.getResponse();
             JSONObject message = new JSONObject();
-            message.put("code", -1);
+            message.put("code", 429);
             message.put("msg", "TOO MANY REQUESTS!");
             byte[] bits = message.toJSONString().getBytes(StandardCharsets.UTF_8);
             DataBuffer buffer = response.bufferFactory().wrap(bits);
